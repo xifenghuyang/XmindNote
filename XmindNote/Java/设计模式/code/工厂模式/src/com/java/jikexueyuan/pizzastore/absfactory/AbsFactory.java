89ff4f66a79130ca1610c61666b6e62ca0ee5764
yangqiangxidian@163.com <yangqiangxidian@163.com>
2019-07-02 00:50:48 +0800

    设计模式：单例模式+工厂模式:简单工厂模式、工厂方法模式 java核心：hashmap

--- XmindNote/Java/设计模式/code/工厂模式/src/com/java/jikexueyuan/pizzastore/absfactory/AbsFactory.java
+++
@@ -1,7 +0,0 @@
-package com.java.jikexueyuan.pizzastore.absfactory;
-
-import com.java.jikexueyuan.pizzastore.pizza.Pizza;
-
-public interface AbsFactory {
-	public Pizza CreatePizza(String ordertype) ;
-}
