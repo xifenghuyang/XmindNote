f27e2999693e919b3494021b3222457ad012cb5e
yangqiangxidian@163.com <yangqiangxidian@163.com>
2019-08-11 01:05:20 +0800

    effectivejava:第五章 泛型

---
+++ XmindNote/EffectiveJava/effective-java-3e-source-code-master/src/effectivejava/chapter5/item26/Raw.java
@@ -0,0 +1,18 @@
+package effectivejava.chapter5.item26;
+import java.util.*;
+
+// Fails at runtime - unsafeAdd method uses a raw type (List)!  (Page 119)
+public class Raw {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        unsafeAdd(strings, Integer.valueOf(42));
+        String s = strings.get(0); // Has compiler-generated cast
+        Set<String> e=new HashSet();
+    }
+
+//    private static void unsafeAdd(List<String> list, Object o) {
+    private static void unsafeAdd(List list, Object o) {
+        list.add(o);
+    }
+}
+
