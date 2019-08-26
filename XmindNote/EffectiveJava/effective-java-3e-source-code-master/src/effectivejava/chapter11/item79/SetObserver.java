6f3e8ef8e10a01d2ffebe30c27379599287e12ba
yangqiangxidian@163.com <yangqiangxidian@163.com>
2019-08-27 00:00:05 +0800

    effectivejava:第十一章，并发

---
+++ XmindNote/EffectiveJava/effective-java-3e-source-code-master/src/effectivejava/chapter11/item79/SetObserver.java
@@ -0,0 +1,8 @@
+package effectivejava.chapter11.item79;
+
+// Set obeserver callback interface - Page 266
+@FunctionalInterface
+public interface SetObserver<E> {
+    // Invoked when an element is added to the observable set
+    void added(ObservableSet<E> set, E element);
+}
